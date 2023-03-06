2a2c26938816e7ffd7bc60228b49c24f0a0efad4
soundsofmath <soundsofmath@gmail.com>
2023-03-05 20:45:24 -0500

    Testing

--- leia/plugin.program.atomwizard/uservar.py
+++ leia/plugin.program.atomwizard/uservar.py
@@ -60,18 +60,18 @@ SPACER = '='
 
 # You can edit these however you want, just make sure that you have a %s in each of the
 # THEME's so it grabs the text from the menu item
-COLOR1 = 'white'
-COLOR2 = 'white'
-# Primary menu items   / {0} is the menu item and is required
-THEME1 = u'[COLOR {color1}][I]([COLOR {color1}][B][/B][/COLOR][COLOR {color2}][COLOR {color1}])[/I][/COLOR] [COLOR {color2}]{{}}[/COLOR]'.format(color1=COLOR1, color2=COLOR2)
-# Build Names          / {0} is the menu item and is required
-THEME2 = u'[COLOR {color1}]{{}}[/COLOR]'.format(color1=COLOR1)
-# Alternate items      / {0} is the menu item and is required
-THEME3 = u'[COLOR {color1}]{{}}[/COLOR]'.format(color1=COLOR1)
-# Current Build Header / {0} is the menu item and is required
-THEME4 = u'[COLOR {color1}]Current Build:[/COLOR] [COLOR {color2}]{{}}[/COLOR]'.format(color1=COLOR1, color2=COLOR2)
-# Current Theme Header / {0} is the menu item and is required
-THEME5 = u'[COLOR {color1}]Current Theme:[/COLOR] [COLOR {color2}]{{}}[/COLOR]'.format(color1=COLOR1, color2=COLOR2)
+COLOR1         = 'dodgerblue'
+COLOR2         = 'white'
+# Primary menu items   / %s is the menu item and is required
+THEME1         = '[COLOR 'COLOR1'][B][I]([COLOR 'COLOR2']Aftermath[/COLOR])[/B][/COLOR] [COLOR 'COLOR2']%s[/COLOR][/I]'
+# Build Names          / %s is the menu item and is required
+THEME2         = '[COLOR 'COLOR2']%s[/COLOR]'
+# Alternate items      / %s is the menu item and is required
+THEME3         = '[COLOR 'COLOR1']%s[/COLOR]'
+# Current Build Header / %s is the menu item and is required
+THEME4         = '[COLOR 'COLOR1']Current Build:[/COLOR] [COLOR 'COLOR2']%s[/COLOR]'
+# Current Theme Header / %s is the menu item and is required
+THEME5         = '[COLOR 'COLOR1']Current Theme:[/COLOR] [COLOR 'COLOR2']%s[/COLOR]'
 
 # Message for Contact Page
 # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
