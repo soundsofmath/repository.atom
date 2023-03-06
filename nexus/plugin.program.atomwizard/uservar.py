08cd1542afcdc79eb41245192c59676479c307b5
soundsofmath <soundsofmath@gmail.com>
2023-03-06 05:32:46 -0500

    Proper Preparation Prevents Poor Performance

--- nexus/plugin.program.atomwizard/uservar.py
+++ nexus/plugin.program.atomwizard/uservar.py
@@ -14,10 +14,10 @@ ART = os.path.join(PATH, 'resources', 'media')
 #        User Edit Variables                            #
 #########################################################
 ADDONTITLE = 'Atom Wizard'
-BUILDERNAME = 'Matrix Builds'
+BUILDERNAME = 'Nexus Builds'
 EXCLUDES = [ADDON_ID, 'plugin.program.atomwizard']
 # Text File with build info in it. Please read https://github.com/a4k-openproject/plugin.program.openwizard/wiki/Installing-Builds
-BUILDFILE = 'https://raw.githubusercontent.com/soundsofmath/repository.atom/master/builds.txt'
+BUILDFILE = 'https://raw.githubusercontent.com/soundsofmath/repository.atom/master/buildwiz.txt'
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK = 0
