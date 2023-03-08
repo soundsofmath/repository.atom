b5582fccd32a00dfdc55cc0fe0c66e80314d2a45
soundsofmath <soundsofmath@gmail.com>
2023-03-08 16:14:30 -0500

    Testing

--- nexus/plugin.program.atomwizard/uservar.py
+++ nexus/plugin.program.atomwizard/uservar.py
@@ -17,7 +17,7 @@ ADDONTITLE = 'Atom Wizard'
 BUILDERNAME = 'Nexus Builds'
 EXCLUDES = [ADDON_ID, 'plugin.program.atomwizard']
 # Text File with build info in it. Please read https://github.com/a4k-openproject/plugin.program.openwizard/wiki/Installing-Builds
-BUILDFILE = 'https://raw.githubusercontent.com/soundsofmath/repository.atom/master/buildwiz.txt'
+BUILDFILE = 'https://raw.githubusercontent.com/soundsofmath/repository.atom/master/wiz/buildwiz.txt'
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK = 0
